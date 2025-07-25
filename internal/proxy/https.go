253d65f1d59c9c49ddc6d910445870aeb51c7ef8
iTrooz <hey@itrooz.fr>
2025-07-26 00:24:49 +0200

    move HTTPS specific functions to https.go

---
+++ internal/proxy/https.go
@@ -0,0 +1,93 @@
+package proxy
+
+import (
+	"caching-dev-proxy/internal/config"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/url"
+
+	"github.com/elazarl/goproxy"
+	"github.com/inconshreveable/go-vhost"
+	"github.com/sirupsen/logrus"
+)
+
+func loadCertificate(cfg *config.Config) (*tls.Certificate, error) {
+	if cfg.Server.HTTPS.CACertFile == "" || cfg.Server.HTTPS.CAKeyFile == "" {
+		logrus.Debugf("No CA certificate configured, using goproxy default certificate")
+		return nil, nil // Use default goproxy certificate
+	}
+
+	cert, err := tls.LoadX509KeyPair(cfg.Server.HTTPS.CACertFile, cfg.Server.HTTPS.CAKeyFile)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load CA certificate and key: %w", err)
+	}
+	logrus.Debugf("Loaded CA certificate from %s", cfg.Server.HTTPS.CACertFile)
+	return &cert, nil
+}
+
+func (s *Server) setupHTTPSProxyHandler() {
+	// Load CA certificate
+	caCert, err := loadCertificate(s.config)
+	if err != nil {
+		logrus.Errorf("Failed to load CA certificate: %v", err)
+		return
+	}
+
+	if caCert == nil {
+		// Use goproxy's default certificate
+		logrus.Warnf("TLS interception enabled but no CA certificate loaded, using goproxy default certificate")
+		s.proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
+	} else {
+		// Make goproxy use our provided CA certificate
+		customCaMitm := &goproxy.ConnectAction{
+			Action:    goproxy.ConnectMitm,
+			TLSConfig: goproxy.TLSConfigFromCA(caCert),
+		}
+		customAlwaysMitm := goproxy.FuncHttpsHandler(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
+			logrus.Debugf("Handling CONNECT request for %s", host)
+			return customCaMitm, host
+		})
+		s.proxy.OnRequest().HandleConnect(customAlwaysMitm)
+	}
+}
+
+// StartTransparentHTTPS enables transparent HTTPS proxying
+func (s *Server) StartTransparentHTTPS(httpsAddr string) {
+	ln, err := net.Listen("tcp", httpsAddr)
+	if err != nil {
+		log.Fatalf("Error listening for https connections - %v", err)
+	}
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting new connection - %v", err)
+			continue
+		}
+		go func(c net.Conn) {
+			tlsConn, err := vhost.TLS(c)
+			if err != nil {
+				log.Printf("Error accepting new connection - %v", err)
+				return
+			}
+			if tlsConn.Host() == "" {
+				log.Printf("Cannot support non-SNI enabled clients")
+				return
+			}
+			connectReq := &http.Request{
+				Method: http.MethodConnect,
+				URL: &url.URL{
+					Opaque: tlsConn.Host(),
+					Host:   net.JoinHostPort(tlsConn.Host(), "443"),
+				},
+				Host:       tlsConn.Host(),
+				Header:     make(http.Header),
+				RemoteAddr: c.RemoteAddr().String(),
+			}
+			resp := dumbResponseWriter{tlsConn}
+			s.proxy.ServeHTTP(resp, connectReq)
+		}(c)
+	}
+}
--- internal/proxy/server.go
+++ internal/proxy/server.go
@@ -6,9 +6,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net"
 	"net/http"
-	"net/url"
 	"os"
 	"time"
 
@@ -16,7 +14,6 @@ import (
 	"caching-dev-proxy/internal/config"
 
 	"github.com/elazarl/goproxy"
-	"github.com/inconshreveable/go-vhost"
 	"github.com/sirupsen/logrus"
 )
 
@@ -124,46 +121,6 @@ func copyResponse(resp *http.Response) (*http.Response, error) {
 	resp.Body = io.NopCloser(bytes.NewReader(bodyBytes))
 
 	return &respCopy, nil
-}
-
-func loadCertificate(cfg *config.Config) (*tls.Certificate, error) {
-	if cfg.Server.HTTPS.CACertFile == "" || cfg.Server.HTTPS.CAKeyFile == "" {
-		logrus.Debugf("No CA certificate configured, using goproxy default certificate")
-		return nil, nil // Use default goproxy certificate
-	}
-
-	cert, err := tls.LoadX509KeyPair(cfg.Server.HTTPS.CACertFile, cfg.Server.HTTPS.CAKeyFile)
-	if err != nil {
-		return nil, fmt.Errorf("failed to load CA certificate and key: %w", err)
-	}
-	logrus.Debugf("Loaded CA certificate from %s", cfg.Server.HTTPS.CACertFile)
-	return &cert, nil
-}
-
-func (s *Server) setupHTTPSProxyHandler() {
-	// Load CA certificate
-	caCert, err := loadCertificate(s.config)
-	if err != nil {
-		logrus.Errorf("Failed to load CA certificate: %v", err)
-		return
-	}
-
-	if caCert == nil {
-		// Use goproxy's default certificate
-		logrus.Warnf("TLS interception enabled but no CA certificate loaded, using goproxy default certificate")
-		s.proxy.OnRequest().HandleConnect(goproxy.AlwaysMitm)
-	} else {
-		// Make goproxy use our provided CA certificate
-		customCaMitm := &goproxy.ConnectAction{
-			Action:    goproxy.ConnectMitm,
-			TLSConfig: goproxy.TLSConfigFromCA(caCert),
-		}
-		customAlwaysMitm := goproxy.FuncHttpsHandler(func(host string, ctx *goproxy.ProxyCtx) (*goproxy.ConnectAction, string) {
-			logrus.Debugf("Handling CONNECT request for %s", host)
-			return customCaMitm, host
-		})
-		s.proxy.OnRequest().HandleConnect(customAlwaysMitm)
-	}
 }
 
 // setupProxyHandlers configures the goproxy handlers
@@ -338,43 +295,5 @@ func (s *Server) shouldBeCached(requ *http.Request, resp *http.Response) bool {
 func (s *Server) cacheResponse(requ *http.Request, resp *http.Response) {
 	if err := s.cacheManager.Set(requ, resp); err != nil {
 		logrus.Errorf("Failed to cache response for %s: %v", requ.URL.String(), err)
-	}
-}
-
-// StartTransparentHTTPS enables transparent HTTPS proxying
-func (s *Server) StartTransparentHTTPS(httpsAddr string) {
-	ln, err := net.Listen("tcp", httpsAddr)
-	if err != nil {
-		log.Fatalf("Error listening for https connections - %v", err)
-	}
-	for {
-		c, err := ln.Accept()
-		if err != nil {
-			log.Printf("Error accepting new connection - %v", err)
-			continue
-		}
-		go func(c net.Conn) {
-			tlsConn, err := vhost.TLS(c)
-			if err != nil {
-				log.Printf("Error accepting new connection - %v", err)
-				return
-			}
-			if tlsConn.Host() == "" {
-				log.Printf("Cannot support non-SNI enabled clients")
-				return
-			}
-			connectReq := &http.Request{
-				Method: http.MethodConnect,
-				URL: &url.URL{
-					Opaque: tlsConn.Host(),
-					Host:   net.JoinHostPort(tlsConn.Host(), "443"),
-				},
-				Host:       tlsConn.Host(),
-				Header:     make(http.Header),
-				RemoteAddr: c.RemoteAddr().String(),
-			}
-			resp := dumbResponseWriter{tlsConn}
-			s.proxy.ServeHTTP(resp, connectReq)
-		}(c)
 	}
 }
