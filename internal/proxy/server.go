72bd55d246834f959718fc187adaca487edba970
iTrooz <hey@itrooz.fr>
2025-07-26 00:30:34 +0200

    put simpleCertStore in cert_store.go

--- internal/proxy/server.go
+++ internal/proxy/server.go
@@ -17,27 +17,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-// simpleCertStore implements goproxy.CertStorage for certificate caching
-type simpleCertStore struct {
-	certs map[string]*tls.Certificate
-}
-
-func (s *simpleCertStore) Fetch(hostname string, gen func() (*tls.Certificate, error)) (*tls.Certificate, error) {
-	cert, ok := s.certs[hostname]
-	if ok {
-		return cert, nil
-	}
-
-	cert, err := gen()
-	if err != nil {
-		logrus.Errorf("Failed to generate certificate for hostname '%s': %v", hostname, err)
-		return nil, fmt.Errorf("failed to generate certificate for hostname '%s': %w", hostname, err)
-	}
-
-	s.certs[hostname] = cert
-	return cert, nil
-}
-
 // ProxyResponse holds the response data from upstream
 type ProxyResponse struct {
 	StatusCode int
